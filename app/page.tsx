a8a6c89b2ee04d294d24363cde64b6db0f87c3a3
Ishaan-03 <saxenaishaan03@gmail.com>
2024-10-29 15:02:16 +0530

    fixed few more deployment issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import AcmeLogo from '@/app/ui/moneymap';
+import AcmeLogo from '@/app/ui/acme';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from './ui/font';
