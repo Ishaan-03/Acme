9e24b91821401a9e4a9bf7e11dc208221b7eed7c
Ishaan-03 <saxenaishaan03@gmail.com>
2024-10-30 12:57:07 +0530

    completed the dashboard page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+export  const nextConfig: NextConfig = {
+  experimental: {
+    ppr: 'incremental',
+  },
 };
-
-export default nextConfig;
