543829b7390d7e5227d7427345307d27ba06b9ef
Ishaan-03 <saxenaishaan03@gmail.com>
2024-11-01 14:41:22 +0530

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,7 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to MoneyMap.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to acme.</strong> it is a financial tracking dashboard for businesses to manage transactions and lendings, including pending and paid transactions.{' '}
           </p>
           <Link
             href="/login"
